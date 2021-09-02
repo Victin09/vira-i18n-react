e62b7c7abd12b2359d7a7277899197c258e8b6cf
Victor Gomez <victorgomez1209@outlook.es>
2021-09-02 21:33:47 +0200

    chnage provider props

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { I18n } from 'vira-i18n';
 
 import { I18nContextProps, I18nProviderProps } from './types/i18n.types';
@@ -8,42 +8,45 @@ const I18Context = createContext<I18nContextProps>({
     locales: [],
     
     i18n: () => '',
-    setLanguage: () => { },
+    setLanguage: () => {},
 });
 
 export const I18nProvider: React.FC<I18nProviderProps> = ({ children, language, locales }) => {
-    const stateHook = React.useState(language);
+    const [lang, setLang] = useState(language);
 
     useEffect(() => {
-        stateHook[1](language);
+        setLang(language);
     }, [language]);
 
-    const translator = new I18n(stateHook[0], locales);
+    const translator = new I18n(lang, locales);
 
     const i18n = (resourcesKey: string) => {
         return translator.i18n(resourcesKey);
     }
 
+    const setLanguage = (language: string) => {
+        setLang(language);
+    }
+
     return (
         <I18Context.Provider
             value={{
-                language: stateHook[0],
+                language: lang,
                 locales,
                 i18n,
-                setLanguage: stateHook[1],
+                setLanguage,
             }}>
             {children}
         </I18Context.Provider>
     );
 };
 
-export function useI18n(language?: string) {
+export const useI18n = (): I18nContextProps => {
     const context = useContext(I18Context);
 
-    if (language && language !== context.language) {
-        return new I18n(language);
-    }
-
+    if (context === undefined)
+        throw new Error('I18n context is undefined');
+        
     return context;
 }
 
